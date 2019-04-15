cb01b8b8d26bd186385160053ad19b82ae3319c2
tracer-ics2017 <tracer@njuics.org>
2019-04-15 22:33:59 +0800

    >  compile 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  22:33:58 up 58 min,  1 user,  load average: 0.12, 0.04, 0.01 725ddb2c80ea559d5c81fef64a2fe4299deab01a

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -49,11 +49,11 @@ void free_wp(int num){
 }
 
 void info_w(){
-	printf("NO Old Value  Expr\n");
 	if(head == NULL){
 		printf("There is no watchpoint in pool!\n");
 		assert(0);
 	}
+	printf("NO Old Value  Expr\n");
 	WP *p=head,*q;
 	for(q=head;q!=NULL;q=q->next);
 	while(q != head->next){
