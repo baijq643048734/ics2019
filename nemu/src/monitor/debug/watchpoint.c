fab943e4bf33db889457a2cb2fb025da5d1351db
tracer-ics2017 <tracer@njuics.org>
2019-04-17 00:42:10 +0800

    >  compile 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:42:10 up  3:18,  1 user,  load average: 0.14, 0.05, 0.01 b694dde129955ddbc8b96e133ba4ae28b5805e8d

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -91,12 +91,14 @@ WP* scan_watchpoint(void){
 	bool success;
 //	bool is_hit = false;
 	WP *p=head;
+	int result;
 	for(;p!=NULL;p=p->next){
-		p -> new_val = expr(p -> expr,&success);
-		if(p -> new_val != p -> old_val){
+		result = expr(p -> expr,&success);
+		if(result!= p -> old_val){
+			p->new_val = result;
 			printf("Hit watchpoint %d at address 0x%08x\n",p->NO,p->old_val);
 			printf("666\n");
-			p->old_val = p->new_val;
+			p->old_val = result;
 			return p;
 //			is_hit = true;
 		}
