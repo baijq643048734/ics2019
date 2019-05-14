be1054de82b30a616ce498fa0f9b8f08d861969d
tracer-ics2017 <tracer@njuics.org>
2019-05-14 21:06:52 +0800

    >  compile 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  21:06:52 up  8:47,  2 users,  load average: 0.02, 0.02, 0.00 c35c6bde23ab6e37fb38a324b9f60c7a61c91cc2

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -44,3 +44,4 @@ make_EHelper(lea);
 make_EHelper(nop);
 make_EHelper(call_rm);
 make_EHelper(jmp_rm);
+make_EHelper(setcc);
--- nemu/src/cpu/exec/cc.c
+++ nemu/src/cpu/exec/cc.c
@@ -15,13 +15,33 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
-    case CC_B:
-    case CC_E:
+		if(cpu.eflags.OF==1) *dest = 1;
+		else *dest = 0;
+		break;
+	case CC_B:
+		if(cpu.eflags.CF==1) *dest = 1;
+		else *dest = 0;
+		break;
+	case CC_E:
+		if(cpu.eflags.ZF==1) *dest = 1;
+		else *dest = 0;
+		break;
     case CC_BE:
+		if(cpu.eflags.CF||cpu.eflags.ZF) *dest = 1;
+		else *dest = 0;
+		break;
     case CC_S:
+		if(cpu.eflags.SF==1) *dest = 1;
+		else *dest = 0;
+		break;
     case CC_L:
+		if(cpu.eflags.SF!=cpu.eflags.OF) *dest = 1;
+		else *dest = 0;
+		break;
     case CC_LE:
-      TODO();
+		if(cpu.eflags.OF!=cpu.eflags.SF || cpu.eflags.ZF) *dest = 1;
+		else *dest = 0;
+		break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
