a6490c7a9b2a3d372e064d1dbc48ccc4f2ef4398
tracer-ics2017 <tracer@njuics.org>
2019-05-15 14:26:32 +0800

    >  compile 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  14:26:32 up  3:09,  1 user,  load average: 0.00, 0.26, 0.40 adadd2ac80f35cfc17724da35c171514b62edb45

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -1,5 +1,5 @@
 #include "nemu.h"
-
+#include "device/mmio.h"
 #define PMEM_SIZE (128 * 1024 * 1024)
 
 #define pmem_rw(addr, type) *(type *)({\
@@ -12,10 +12,16 @@ uint8_t pmem[PMEM_SIZE];
 /* Memory accessing interfaces */
 
 uint32_t paddr_read(paddr_t addr, int len) {
+	int mmio_id;
+	if((mmio_id = is_mmio(addr))!=-1) return mmio_read(addr,len,mmio_id);
+	else
   return pmem_rw(addr, uint32_t) & (~0u >> ((4 - len) << 3));
 }
 
 void paddr_write(paddr_t addr, int len, uint32_t data) {
+	int mmio_id;
+	if((mmio_id=is_mmio(addr))!=-1) mmio_write(addr,len,data,mmio_id);
+	else
   memcpy(guest_to_host(addr), &data, len);
 }
 
