1367cc1458545c42ab8099a5dc1381d7cdd77a41
tracer-ics2017 <tracer@njuics.org>
2019-06-03 19:36:12 +0800

    >  run 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  19:36:12 up 48 min,  1 user,  load average: 0.09, 0.03, 0.00 5bfe4e828c6edf9678a0308743f8af37b7beabc4

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -3,6 +3,7 @@
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  size_t size = get_ramdisk_size();
+  ramdisk_read(DEFAULT_ENTRY,0,size);
   return (uintptr_t)DEFAULT_ENTRY;
 }
