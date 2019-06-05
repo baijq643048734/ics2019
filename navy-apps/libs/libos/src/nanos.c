54eaf5530309bff78880dece8776de8e5509e6b3
tracer-ics2017 <tracer@njuics.org>
2019-06-05 10:41:44 +0800

    >  run 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  10:41:44 up 46 min,  1 user,  load average: 0.08, 0.02, 0.00 21bc85deb3088e1b3c9966e5458b81d08df82a9c

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -26,8 +26,7 @@ int _open(const char *path, int flags, mode_t mode) {
 }
 
 int _write(int fd, void *buf, size_t count){
-//  return _syscall_(SYS_write,fd,(uintptr_t)buf,count);
-	_exit(SYS_write);
+  return _syscall_(SYS_write,fd,(uintptr_t)buf,count);
 }
 
 extern char _end;
