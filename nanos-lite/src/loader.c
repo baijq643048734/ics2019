220f741c1711f0a93ace4d235b31ea4fa372e7b9
tracer-ics2017 <tracer@njuics.org>
2019-06-06 15:40:08 +0800

    >  run 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:40:08 up  1:30,  1 user,  load average: 0.04, 0.18, 0.09 f4654524df538b773374fed328e345af6d0cfaff

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,7 +1,8 @@
 #include "common.h"
 #include "fs.h"
 #include "memory.h"
-#define DEFAULT_ENTRY ((void *)0x4000000)
+//#define DEFAULT_ENTRY ((void *)0x4000000)
+#define DEFAULT_ENTRY ((void*)0x8048000)
 
 extern void ramdisk_read(void *buf,off_t offset,size_t len);
 extern size_t get_ramdisk_size();
