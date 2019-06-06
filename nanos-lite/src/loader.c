e37da443bc3acd6f6acb015b9209760e3355fa2d
tracer-ics2017 <tracer@njuics.org>
2019-06-06 15:33:33 +0800

    >  run 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:33:33 up  1:24,  1 user,  load average: 0.02, 0.02, 0.00 4cdf015394f70655eb15bdd5124e64035ca22232

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,22 +1,30 @@
 #include "common.h"
+#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 extern void ramdisk_read(void *buf,off_t offset,size_t len);
 extern size_t get_ramdisk_size();
 
-extern ssize_t fs_read(int fd,void *buf,size_t len);
-extern size_t fs_filesz(int fd);
-extern int fs_open(const char *pathname,int flags,int mode);
-extern int fs_close(int fd);
+//extern ssize_t fs_read(int fd,void *buf,size_t len);
+//extern size_t fs_filesz(int fd);
+//extern int fs_open(const char *pathname,int flags,int mode);
+//extern int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
 //  size_t size = get_ramdisk_size();
 //  ramdisk_read(DEFAULT_ENTRY,0,size);
 	
 	int fd = fs_open(filename,0,0);
+//	size_t nbyte = fs_filesz(fd);
+//	void *pa,*va;
+//	void *end = DEFAULT_ENTRY + nbyte;
+//	for(va = DEFAULT_ENTRY;va < end ; va+=PGSIZE){
+//		pa = new_page();
+//		_map(as,va,pa);
+//		fs_read(fd,pa,(end - va) < PGSIZE ? (end -va):PGSIZE);
+//	}
 	fs_read(fd,DEFAULT_ENTRY,fs_filesz(fd));
 	fs_close(fd);
-	Log("1");
   return (uintptr_t)DEFAULT_ENTRY;
 }
