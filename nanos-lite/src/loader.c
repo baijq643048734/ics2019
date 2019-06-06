238243eae8a80242853d44f7c4f020828cfa8603
tracer-ics2017 <tracer@njuics.org>
2019-06-06 15:34:12 +0800

    >  run 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  15:34:12 up  1:24,  1 user,  load average: 0.23, 0.06, 0.01 ed32f146fb9508a1f9eb168f5085bbfb503026a7

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -1,15 +1,14 @@
 #include "common.h"
-#include "fs.h"
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
 extern void ramdisk_read(void *buf,off_t offset,size_t len);
 extern size_t get_ramdisk_size();
 
-//extern ssize_t fs_read(int fd,void *buf,size_t len);
-//extern size_t fs_filesz(int fd);
-//extern int fs_open(const char *pathname,int flags,int mode);
-//extern int fs_close(int fd);
+extern ssize_t fs_read(int fd,void *buf,size_t len);
+extern size_t fs_filesz(int fd);
+extern int fs_open(const char *pathname,int flags,int mode);
+extern int fs_close(int fd);
 
 uintptr_t loader(_Protect *as, const char *filename) {
 //  size_t size = get_ramdisk_size();
