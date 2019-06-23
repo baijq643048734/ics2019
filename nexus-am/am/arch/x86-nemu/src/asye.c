7a0e08b53e5b32c5762b3a3f1a317c0a868cde0e
tracer-ics2017 <tracer@njuics.org>
2019-06-24 00:01:50 +0800

    >  run 161720328 baijiaqi Linux debian 4.9.0-8-686 #1 SMP Debian 4.9.130-2 (2018-10-27) i686 GNU/Linux  00:01:50 up  1:51,  1 user,  load average: 0.01, 0.02, 0.00 e2c357fff88abfb0cc48fe7252ee5bb250e65aa

--- nexus-am/am/arch/x86-nemu/src/asye.c
+++ nexus-am/am/arch/x86-nemu/src/asye.c
@@ -5,6 +5,7 @@ static _RegSet* (*H)(_Event, _RegSet*) = NULL;
 
 void vecsys();
 void vecnull();
+//void vectrap();
 
 _RegSet* irq_handle(_RegSet *tf) {
   _RegSet *next = tf;
@@ -35,6 +36,8 @@ void _asye_init(_RegSet*(*h)(_Event, _RegSet*)) {
 
   // -------------------- system call --------------------------
   idt[0x80] = GATE(STS_TG32, KSEL(SEG_KCODE), vecsys, DPL_USER);
+  // -------------------- system trap --------------------------
+//  idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_USER);
 
   set_idt(idt, sizeof(idt));
 
@@ -47,6 +50,7 @@ _RegSet *_make(_Area stack, void *entry, void *arg) {
 }
 
 void _trap() {
+	asm volatile("int $0x81");
 }
 
 int _istatus(int enable) {
